bc7ee602514660495eeacf85454ce2efdd8fff03
TheIttyBittyGalaxy <43608508+TheIttyBittyGalaxy@users.noreply.github.com>
2024-10-22 00:41:31 +0100

    compiler: Clean up arc creation code
    It's still not as good as it could be, but hopefully improving this code further will now be made easier (particularly as my next goal is to support rules that have multiple placeholders)

--- src/constraints.c
+++ src/constraints.c
@@ -4,9 +4,9 @@
 #include "constraints.h"
 #include "memory.h"
 
-// ArcInformation
+// ConversionResult
 // When we are converting a rule into multiple arc constraints, `convert_expression` will take a rule
-// and produce a single expression (to be used by the arc), as well as a `ArcInformation`. `create_constraints`
+// and produce a single expression (to be used by the arc), as well as a `ConversionResult`. `create_constraints`
 // can then use this to create an arc for every set of instances that the rule applies to.
 
 typedef struct
@@ -16,52 +16,30 @@ typedef struct
 
 typedef struct
 {
-    Expression *exprs;
-    size_t exprs_count;
-    VariableInfo *var_info_map;
-    size_t var_info_map_count;
-} ArcInformation; // CLEANUP: Is there a better name for this?
-
-size_t arc_value_index_for(ArcInformation *arc_info, size_t property_offset)
-{
-    for (size_t i = 0; i < arc_info->var_info_map_count; i++)
+    VariableInfo *var_info;
+    union
     {
-        VariableInfo info = arc_info->var_info_map[i];
+        size_t var_info_count;
+        size_t var_count;
+    };
+} ConversionResult; // CLEANUP: Is there a better name for this?
+
+size_t get_variable_index_for(ConversionResult *result, size_t property_offset)
+{
+    for (size_t i = 0; i < result->var_info_count; i++)
+    {
+        VariableInfo info = result->var_info[i];
         if (info.property_offset == property_offset)
             return i;
     }
 
-    VariableInfo *info = EXTEND_ARRAY(arc_info->var_info_map, VariableInfo);
+    VariableInfo *info = EXTEND_ARRAY(result->var_info, VariableInfo);
     info->property_offset = property_offset;
-    return arc_info->var_info_map_count - 1;
+    return result->var_info_count - 1;
 }
 
-// Convert program expression into arc
-
-Expression *convert_expression(ArcInformation *arc_info, Node *node, Expression *program_expression);
-Expression *copy_arc_expr_with_rotation(Expression *original_expr, size_t rotation, size_t variable_count);
-
-ArcInformation convert(Node *node, Expression *program_expression)
-{
-    ArcInformation arc_info;
-    INIT_ARRAY(arc_info.exprs);
-    INIT_ARRAY(arc_info.var_info_map);
-
-    Expression *expr = convert_expression(&arc_info, node, program_expression);
-
-    arc_info.exprs_count = arc_info.var_info_map_count;
-    arc_info.exprs = (Expression *)malloc(sizeof(Expression) * arc_info.exprs_count);
-
-    arc_info.exprs[0] = *expr; // FIXME: This probably leaks memory!!
-    for (size_t i = 1; i < arc_info.exprs_count; i++)
-    {
-        // FIXME: This probably also leaks memory!
-        arc_info.exprs[i] = *copy_arc_expr_with_rotation(expr, i, arc_info.var_info_map_count);
-    }
-    return arc_info;
-}
-
-Expression *convert_expression(ArcInformation *arc_info, Node *node, Expression *program_expression)
+// Convert a rule's expression into an arc expression
+Expression *convert_expression(ConversionResult *result, Node *node, Expression *program_expression)
 {
     Expression *expr = NEW(Expression);
 
@@ -93,7 +71,7 @@ Expression *convert_expression(ArcInformation *arc_info, Node *node, Expression 
                 sub_string other_name = (node->properties + i)->name;
                 if (substrings_match(prop_name, other_name))
                 {
-                    expr->index = arc_value_index_for(arc_info, i);
+                    expr->index = get_variable_index_for(result, i);
                     break;
                 }
             }
@@ -102,8 +80,8 @@ Expression *convert_expression(ArcInformation *arc_info, Node *node, Expression 
         {
             expr->kind = BIN_OP;
             expr->op = program_expression->op;
-            expr->lhs = convert_expression(arc_info, node, program_expression->lhs);
-            expr->rhs = convert_expression(arc_info, node, program_expression->rhs);
+            expr->lhs = convert_expression(result, node, program_expression->lhs);
+            expr->rhs = convert_expression(result, node, program_expression->rhs);
         }
 
         break;
@@ -119,7 +97,8 @@ Expression *convert_expression(ArcInformation *arc_info, Node *node, Expression 
     return expr;
 }
 
-Expression *copy_arc_expr_with_rotation(Expression *original_expr, size_t rotation, size_t variable_count)
+// Create a copy of an arc expresion where each the variable index for each arc value is rotated by a given amount
+Expression *rotate_arc_expression(Expression *original_expr, size_t rotation, size_t variable_count)
 {
     Expression *expr = NEW(Expression);
 
@@ -134,8 +113,8 @@ Expression *copy_arc_expr_with_rotation(Expression *original_expr, size_t rotati
     {
         expr->kind = BIN_OP;
         expr->op = original_expr->op;
-        expr->lhs = copy_arc_expr_with_rotation(original_expr->lhs, rotation, variable_count);
-        expr->rhs = copy_arc_expr_with_rotation(original_expr->rhs, rotation, variable_count);
+        expr->lhs = rotate_arc_expression(original_expr->lhs, rotation, variable_count);
+        expr->rhs = rotate_arc_expression(original_expr->rhs, rotation, variable_count);
         break;
     }
     case ARC_VALUE:
@@ -156,6 +135,81 @@ Expression *copy_arc_expr_with_rotation(Expression *original_expr, size_t rotati
 }
 
 // Create constraints
+void create_arcs_from_rule(Constraints *constraints, Rule *rule, QuantumMap *quantum_map)
+{
+    // TODO: Create arcs for rules with multiple placeholders
+    if (rule->placeholders_count != 1)
+    {
+        printf("Rules with multiple placeholders (or no placeholders?) not yet supported:\n");
+        print_rule(rule);
+        return;
+    }
+
+    // NOTE: The following code is designed for rules with exactly one placeholder.
+    Placeholder *placeholder = rule->placeholders;
+
+    ConversionResult result;
+    INIT_ARRAY(result.var_info);
+
+    // FIXME: The expression that is returned by `convert_expression` never has an "owning"
+    //        reference created for it, meaning it's not clear how it would be freed? Once
+    //        it is clearer how this should be done, fix this!
+    Expression *arc_expression = convert_expression(&result, placeholder->node_type, rule->expression);
+
+    if (result.var_count == 0)
+    {
+        fprintf(stderr, "Internal error: Somehow created an arc that constraints no values");
+        exit(EXIT_FAILURE);
+    }
+
+    // Arcs that constrain a single variable
+    if (result.var_count == 1)
+    {
+        for (size_t i = 0; i < quantum_map->instances_count; i++)
+        {
+            QuantumInstance *instance = quantum_map->instances + i;
+            if (instance->node != placeholder->node_type)
+                continue;
+
+            Arc *arc = EXTEND_ARRAY(constraints->single_arcs, Arc);
+            arc->expr = arc_expression;
+            arc->variable_indexes_count = 1;
+            arc->variable_indexes = (size_t *)malloc(sizeof(size_t));
+            arc->variable_indexes[0] = instance->variables_array_index + result.var_info[0].property_offset;
+        }
+
+        return;
+    }
+
+    // Arcs that constrain a multiple variables
+    Expression *arc_expressions = (Expression *)malloc(sizeof(Expression) * result.var_count);
+
+    // FIXME: This almost certainly causes memory leaks?
+    arc_expressions[0] = *arc_expression;
+    for (size_t rotation = 1; rotation < result.var_count; rotation++)
+        arc_expressions[rotation] = *rotate_arc_expression(arc_expression, rotation, result.var_count);
+
+    for (size_t i = 0; i < quantum_map->instances_count; i++)
+    {
+        QuantumInstance *instance = quantum_map->instances + i;
+        if (instance->node != placeholder->node_type)
+            continue;
+
+        for (size_t rotation = 0; rotation < result.var_count; rotation++)
+        {
+            Arc *arc = EXTEND_ARRAY(constraints->multi_arcs, Arc);
+            arc->expr = arc_expressions + rotation;
+            arc->variable_indexes_count = result.var_count;
+            arc->variable_indexes = (size_t *)malloc(sizeof(size_t) * arc->variable_indexes_count);
+            for (size_t v = 0; v < result.var_count; v++)
+            {
+                size_t n = (v + rotation) % result.var_count;
+                arc->variable_indexes[n] = instance->variables_array_index + result.var_info[v].property_offset;
+            }
+        }
+    }
+}
+
 Constraints create_constraints(Program *program, QuantumMap *quantum_map)
 {
     Constraints constraints;
@@ -163,62 +217,7 @@ Constraints create_constraints(Program *program, QuantumMap *quantum_map)
     INIT_ARRAY(constraints.multi_arcs);
 
     for (size_t i = 0; i < program->rules_count; i++)
-    {
-        Rule *rule = program->rules + i;
-
-        // TODO: Create arcs for rules with multiple placeholders
-        if (rule->placeholders_count != 1)
-        {
-            printf("Rules with multiple placeholders (or no placeholders?) not yet supported:\n");
-            print_rule(rule);
-            continue;
-        }
-
-        // NOTE: The following code is designed for rules with exactly one placeholder.
-        Placeholder *placeholder = rule->placeholders;
-
-        // TODO: The expression that is returned by `convert` never has an "owning" reference
-        //       created for it, meaning it's not clear how it would be freed? Once it is clearer
-        //       how this should be done, fix this!
-        ArcInformation arc_info = convert(placeholder->node_type, rule->expression);
-
-        for (size_t j = 0; j < quantum_map->instances_count; j++)
-        {
-            QuantumInstance *instance = quantum_map->instances + j;
-            if (instance->node != placeholder->node_type)
-                continue;
-
-            // Arcs that constrain a single variable
-            if (arc_info.var_info_map_count == 1)
-            {
-                Arc *arc = EXTEND_ARRAY(constraints.single_arcs, Arc);
-                arc->expr = arc_info.exprs;
-                arc->variable_indexes_count = arc_info.var_info_map_count;
-                arc->variable_indexes = (size_t *)malloc(sizeof(size_t) * arc->variable_indexes_count);
-                for (size_t v = 0; v < arc_info.var_info_map_count; v++)
-                    arc->variable_indexes[v] = instance->variables_array_index + arc_info.var_info_map[v].property_offset;
-            }
-
-            // Arcs that constrain multiple variables
-            else
-            {
-                for (size_t rotation = 0; rotation < arc_info.var_info_map_count; rotation++)
-                {
-                    Arc *arc = EXTEND_ARRAY(constraints.multi_arcs, Arc);
-
-                    arc->expr = arc_info.exprs + rotation;
-                    arc->variable_indexes_count = arc_info.var_info_map_count;
-                    arc->variable_indexes = (size_t *)malloc(sizeof(size_t) * arc->variable_indexes_count);
-
-                    for (size_t v = 0; v < arc_info.var_info_map_count; v++)
-                    {
-                        size_t n = (v + rotation) % arc_info.var_info_map_count;
-                        arc->variable_indexes[n] = instance->variables_array_index + arc_info.var_info_map[v].property_offset;
-                    }
-                }
-            }
-        }
-    }
+        create_arcs_from_rule(&constraints, program->rules + i, quantum_map);
 
     return constraints;
 }
