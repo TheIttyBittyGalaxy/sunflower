d623caa6d8d77f0157c72ff0a7a9a8db0232a905
TheIttyBittyGalaxy <43608508+TheIttyBittyGalaxy@users.noreply.github.com>
2024-10-31 23:02:05 +0000

    compiler: Define EXTEND_ARRAY without g++ extension
    This allows the program to build using the Visual Studio 2022 compiler.

--- src/memory.h
+++ src/memory.h
@@ -7,13 +7,11 @@
     array = NULL;         \
     array##_count = 0;
 
-#define EXTEND_ARRAY(array, type)                                         \
-    ({                                                                    \
-        if (array##_count++ == 0)                                         \
-            array = (type *)malloc(sizeof(type));                         \
-        else                                                              \
-            array = (type *)realloc(array, sizeof(type) * array##_count); \
-        (array + array##_count - 1);                                      \
-    })
+#define EXTEND_ARRAY(array, type)                                               \
+    (                                                                           \
+        (array = (type *)((array##_count++ == 0)                                \
+                              ? malloc(sizeof(type))                            \
+                              : realloc(array, sizeof(type) * array##_count))), \
+        (array + array##_count - 1))
 
 #endif
